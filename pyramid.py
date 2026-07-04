de6e75f9ea7e12daae57c5e58d20d6678ab6e71a
Shanvi <0805shanvi@gmail.com>
2026-07-04 18:04:09 +0530

    pyramid pattern

---
+++ pyramid.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number of rows: "))
+
+for i in range(n):
+    # Print spaces
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+
+    # Print stars
+    for j in range(2 * i + 1):
+        print("*", end=" ")
+
+    print()
